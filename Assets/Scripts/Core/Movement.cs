4880e8d174d738a602ea9cc494c4a218db4c889c
Brinley Zhao <brinleyzhao@gmaill.com>
2022-07-05 14:08:48 -0400

    can use WASD to move as well as click to move

--- Assets/Scripts/Core/Movement.cs
+++ Assets/Scripts/Core/Movement.cs
@@ -9,6 +9,13 @@ namespace Core
     [SerializeField] private AudioSource flyingSFX;
     private Transform _body;
 
+    [SerializeField] private float speed;
+    [SerializeField] private float turnSmoothTime = 0.1f;
+    private float turnSmoothVelocity;
+
+    private NavMeshAgent _navMeshAgent;
+    private CharacterController _characterController;
+
 
     // Use this for initialization
     void Start()
@@ -17,6 +24,9 @@ namespace Core
       //   var path = pathfinder.GetPath();
       //   _body = transform.Find("Body");
       //   StartCoroutine(FollowPath(path));
+
+      _navMeshAgent = GetComponent<NavMeshAgent>();
+      _characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
@@ -27,6 +37,22 @@ namespace Core
         {
           MoveToCursor();
         }
+      }
+
+      //WASD for movement as well
+      float horizontal = speed * Input.GetAxisRaw("Horizontal");
+      float vertical = speed * Input.GetAxisRaw("Vertical");
+
+      Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+      if (direction.magnitude >= 0.1f)
+      {
+        _navMeshAgent.ResetPath();
+        _characterController.Move(direction * speed * Time.deltaTime);
+
+        //rotate player to where it's moving
+        float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+        transform.rotation = Quaternion.Euler(0f, angle, 0f);
       }
     }
 
@@ -43,13 +69,14 @@ namespace Core
 
         if (hasHit)
         {
-          GetComponent<NavMeshAgent>().destination = hit.point;
+          _navMeshAgent.destination = hit.point;
+
 
           if (hit.collider.CompareTag("Ground Block"))
           {
             // print("hit ground");
-            GameObject effect = Instantiate(GameAssets.PointToClickEffect, hit.point + new Vector3(0,0,0), Quaternion.identity);
-
+            GameObject effect = Instantiate(GameAssets.PointToClickEffect, hit.point + new Vector3(0, 0, 0),
+              Quaternion.identity);
           }
         }
       }
