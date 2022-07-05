2c1799e93e40656907f912f15cbd6f73f0c340e9
Brinley Zhao <brinleyzhao@gmaill.com>
2022-07-05 11:19:16 -0400

    tabs in shop view

--- Assets/Scripts/UI/Shop - Ui/ShopUi.cs
+++ Assets/Scripts/UI/Shop - Ui/ShopUi.cs
@@ -1,11 +1,10 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 using Core;
 using Inventories;
-using Inventory;
 using UnityEngine;
 
-namespace UI
+namespace UI.Shop___Ui
 {
   /// <summary>
   /// to be put on the left section of shop UI
@@ -17,19 +16,38 @@ namespace UI
   {
     // [SerializeField] private Transform shopItemsContainer;
 
-    public List<InventoryItem> itemsForSell = new List<InventoryItem>();
+    private List<InventoryItem> allItemsForSell = new List<InventoryItem>();
 
+    public List<InventoryItem> seedsForSell = new List<InventoryItem>();
+
+
+    public List<InventoryItem> candlesForSell = new List<InventoryItem>();
+
+
+    public void BtnTabAllItemsForSell()
+    {
+      SetUpShopList(allItemsForSell);//debug
+    }
+    public void BtnTabSeedForSell()
+    {
+      SetUpShopList(seedsForSell);//debug
+    }
+    public void BtnTabCandleForSell()
+    {
+      SetUpShopList(candlesForSell);//debug
+    }
 
     private void Start()
     {
-      SetUpShopList(itemsForSell);//debug
+      allItemsForSell = candlesForSell.Union(seedsForSell).ToList();
+      SetUpShopList(allItemsForSell);//debug
     }
 
     private void OnEnable()
     {
-      SetUpShopList(itemsForSell);
+      SetUpShopList(allItemsForSell);
       // GameAssets.buyCart
-      //   .SetDisplayedItem(itemsForSell[0]); //when first open, automatically show the first item for sell
+      //   .SetDisplayedItem(allItemsForSell[0]); //when first open, automatically show the first item for sell
     }
 
     private void SetUpShopList(List<InventoryItem> items)
@@ -58,7 +76,7 @@ namespace UI
       // {
       //   Debug.LogError("inspector shopItemsContainer in ShopUi is empty");
       // }
-      if (itemsForSell == null)
+      if (allItemsForSell == null)
       {
         Debug.LogError("no items to be sold");
       }
